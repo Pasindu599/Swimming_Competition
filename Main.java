487eb146eba058a5b2074bb28faf3e40aad30640
Pasindu Sankalpa <105929988+Pasindu599@users.noreply.github.com>
2023-03-30 22:57:48 +0530

    Updated Main.java

--- Main.java
+++ Main.java
@@ -2,19 +2,35 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter number of swimmers: ");
-        int noSwimmers = scan.nextInt();
-
-        Competition competition = new Competition(noSwimmers);
-        competition.joinSwimmers();
-        System.out.println(competition);
-
-        
-        
-        competition.getSwimmers().get(0).checkScoreBoard();
-        competition.getSwimmers().get(0).swim();
-        
-
+    System.out.print("Enter number of swimmers: ");
+    int noSwimmers = scan.nextInt();
+    System.out.print("Enter number of spectators: ");
+    int noSpectators = scan.nextInt();
+    System.out.print("Enter number of judgers: ");
+    int noJudgers = scan.nextInt();
+    System.out.print("Enter number of supporting staff members: ");
+    int noSupportingStaffMembers = scan.nextInt();
+    
+    Competition competition = new Competition(noSwimmers, noSpectators, 
+    noJudgers,noSupportingStaffMembers );
+    competition.joinSwimmers();// collect data of swimmwers
+    competition.joinSpectators();// collect data of spectators
+    competition.joinJudges();// collect data of judgers
+    competition.joinStaffMembers();// collect data of staff members
+    
+    // Examples get swimmer name and id
+    System.out.println("Name is " + competition.getSwimmers().get(0).getName());
+    System.out.println("Id is " + competition.getSwimmers().get(0).getId());
+    // call swimmer behaviors
+    competition.getSwimmers().get(0).checkScoreBoard();
+    competition.getSwimmers().get(0).swim();
+    // call spectator behaviors
+    competition.getSpectators().get(0).checkScoreBoard();
+    // call judger behaviors
+    competition.getJudgers().get(0).blow();
+    competition.getJudgers().get(0).checkScoreBoard();
+    // call supportingStaffMember behaviors
+    competition.getSupportingStaffMembers().get(0).checkScoreBoard();
 
     }
 }
